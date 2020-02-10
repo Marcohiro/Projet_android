f9fb6a7c3713579837c60f3b7396355e9ec11166
Marcohiro <petit.marcalain@yahoo.fr>
2020-02-10 16:03:16 +0100

    Add multiple folders

---
+++ app/src/main/java/com/projetESGI/projet/LocalFiles/LocalFilesParser.java
@@ -0,0 +1,16 @@
+package com.projetESGI.projet.LocalFiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projetESGI.projet.R;
+
+public class LocalFilesParser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_files_parser);
+    }
+}
---
+++ app/src/main/java/com/projetESGI/projet/MainScreen/MainScreen.java
@@ -0,0 +1,44 @@
+package com.projetESGI.projet.MainScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.projetESGI.projet.LocalFiles.LocalFilesParser;
+import com.projetESGI.projet.OnlineFiles.OnlineFilesParser;
+import com.projetESGI.projet.R;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        findViewById(R.id.onlineFiles).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToOnlineFiles(v);
+            }
+        });
+
+        findViewById(R.id.switchLocalView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToLocalFiles(v);
+            }
+        });
+    }
+
+    private void switchToLocalFiles(View v) {
+        startActivity(new Intent(this, LocalFilesParser.class));
+        finish();
+    }
+
+    private void switchToOnlineFiles(View v) {
+        startActivity(new Intent(this, OnlineFilesParser.class));
+        finish();
+    }
+}

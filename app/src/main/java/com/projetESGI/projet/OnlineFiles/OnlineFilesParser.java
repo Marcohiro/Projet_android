88bdee1451f03f516f2dc7644ca10ac698af7ea3
Marcohiro <petit.marcalain@yahoo.fr>
2020-02-10 16:05:16 +0100

    Ajout googleservice json

---
+++ app/src/main/java/com/projetESGI/projet/OnlineFiles/OnlineFilesParser.java
@@ -0,0 +1,16 @@
+package com.projetESGI.projet.OnlineFiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projetESGI.projet.R;
+
+public class OnlineFilesParser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_files_parser);
+    }
+}

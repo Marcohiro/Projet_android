5d52406e5583d6f52667969451baa90ee9edc1b3
amina saidi <asaidi5@myges.fr>
2020-02-11 10:07:41 +0100

    file manager

--- app/src/main/java/com/projetESGI/projet/MainActivity.java
+++ app/src/main/java/com/projetESGI/projet/MainActivity.java
@@ -2,10 +2,22 @@ package com.projetESGI.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.projetESGI.projet.LocalFiles.LocalFile;
+import com.projetESGI.projet.LocalFiles.LocalFilesParser;
 import com.projetESGI.projet.Users.UsersContent.UsersItem;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 /*
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveClient;
@@ -16,12 +28,31 @@ import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;*/
 
-public class MainActivity extends AppCompatActivity implements ProjectFragment.OnListFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements ProjectFragment.OnListFragmentInteractionListener {
 
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lunch(v);
+            }
+        });
+
+
+
+    }
+
+    public void lunch(View view) {
+        Intent intent = new Intent(this, LocalFile.class);
+        startActivity(intent);
     }
 
     @Override
